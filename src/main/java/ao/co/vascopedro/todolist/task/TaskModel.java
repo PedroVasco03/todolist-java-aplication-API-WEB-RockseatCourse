0388462381eb776e6e2a4a2ef7e0a6cba32445bd
PedroVasco03 <baltazarpedro092@gmail.com>
2023-10-16 18:34:20 +0100

    try catch

--- src/main/java/ao/co/vascopedro/todolist/task/TaskModel.java
+++ src/main/java/ao/co/vascopedro/todolist/task/TaskModel.java
@@ -43,4 +43,10 @@ public class TaskModel {
      @CreationTimestamp
      private LocalDateTime createdAt;
 
+    public void setTitle(String title) throws Exception{
+        if (title.length() > 50){
+            throw new Exception("title must be at least 50 character");
+        }
+        this.title = title;
+    }
 }
